d7916c5d5101fdfae8a57a7deb20726485ef01c1
gushags <jeff@hagkull.com>
2025-12-05 10:34:59 -0800

    Style: animate victory message and leaderboard

--- src/components/Leaderboard/Leaderboard.jsx
+++ src/components/Leaderboard/Leaderboard.jsx
@@ -30,16 +30,35 @@ function Leaderboard({
 
   useEffect(() => {
     // Trigger fade-in on mount
-    const timer = setTimeout(() => setVisible(true), 10);
-    return () => clearTimeout(timer);
+    setTimeout(() => {
+      const timer = setTimeout(() => setVisible(true), 10);
+      return () => clearTimeout(timer);
+    }, 1000); // Delay to allow previous popup to close
   }, []);
 
   return (
     <>
       <div className={`${style.overlay} ${visible ? style.show : ''}`}>
         <div className={style.articleContainer}>
-          <h2>Top Scores</h2>
-          <button onClick={handleNewPlay}>Play Again</button>
+          <div className={style.winner}>
+            <h1>You WIN!</h1>
+          </div>
+          <div className={style.leaderboard}>
+            <h2>Top Scores</h2>
+            <ol>
+              <li>Trent ...... 00:45</li>
+              <li>Bill ...... 00:55</li>
+              <li>Amber ...... 01:05</li>
+              <li>Jeff ...... 01:07</li>
+              <li>Jeff ...... 01:07</li>
+              <li>Jeff ...... 01:09</li>
+              <li>Jeff ...... 01:17</li>
+              <li>Amber ...... 02:07</li>
+              <li>Sam ...... 02:17</li>
+              <li>Elliot ...... 02:25</li>
+            </ol>
+            <button onClick={handleNewPlay}>Play Again</button>
+          </div>
         </div>
       </div>
     </>
